95217fe29a94eb7c43b2435d733ac10ec5ea8dba
idev74 <idevassy77@gmail.com>
2025-03-26 14:00:03 -0700

    first solutions

---
+++ dynamic_array.py
@@ -0,0 +1,68 @@
+'''
+Design a Dynamic Array (aka a resizable array) class, such as an ArrayList in Java or a vector in C++.
+
+Your DynamicArray class should support the following operations:
+
+DynamicArray(int capacity) will initialize an empty array with a capacity of capacity, where capacity > 0.
+int get(int i) will return the element at index i. Assume that index i is valid.
+void set(int i, int n) will set the element at index i to n. Assume that index i is valid.
+void pushback(int n) will push the element n to the end of the array.
+int popback() will pop and return the element at the end of the array. Assume that the array is non-empty.
+void resize() will double the capacity of the array.
+int getSize() will return the number of elements in the array.
+int getCapacity() will return the capacity of the array.
+If we call void pushback(int n) but the array is full, we should resize the array first.
+#
+'''
+
+# Solution
+# Dynamic Array implementation
+# Note: Python lists are dynamic arrays by default,
+# but this is an example of what's going on under the hood.
+class DynamicArray:
+    
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.length = 0
+        self.arr = [0] * self.capacity
+
+    # Get value at i-th index
+    def get(self, i: int) -> int:
+        return self.arr[i]
+
+    # Set n at i-th index
+    def set(self, i: int, n: int) -> None:
+        self.arr[i] = n
+
+    # Insert n in the last position of the array
+    def pushback(self, n: int) -> None:
+        if self.length == self.capacity:
+            self.resize()
+            
+        # insert at next empty position
+        self.arr[self.length] = n
+        self.length += 1
+
+    # Remove the last element in the array
+    def popback(self) -> int:
+        if self.length > 0:
+            # soft delete the last element
+            self.length -= 1
+        # return the popped element
+        return self.arr[self.length]
+
+    def resize(self) -> None:
+        # Create new array of double capacity
+        self.capacity = 2 * self.capacity
+        new_arr = [0] * self.capacity 
+        
+        # Copy elements to new_arr
+        for i in range(self.length):
+            new_arr[i] = self.arr[i]
+        self.arr = new_arr
+
+    def getSize(self) -> int:
+        return self.length
+    
+    def getCapacity(self) -> int:
+        return self.capacity
---
+++ singly_linked_list.py
@@ -0,0 +1,72 @@
+'''
+Design Singly Linked List
+Design a Singly Linked List class.
+
+Your LinkedList class should support the following operations:
+
+LinkedList() will initialize an empty linked list.
+int get(int i) will return the value of the ith node (0-indexed). If the index is out of bounds, return -1.
+void insertHead(int val) will insert a node with val at the head of the list.
+void insertTail(int val) will insert a node with val at the tail of the list.
+bool remove(int i) will remove the ith node (0-indexed). If the index is out of bounds, return false, otherwise return true.
+int[] getValues() return an array of all the values in the linked list, ordered from head to tail.
+'''
+
+# Solution
+# Singly Linked List Node
+class ListNode:
+    def __init__(self, val, next_node=None):
+        self.val = val
+        self.next = next_node
+
+# Implementation for Singly Linked List
+class LinkedList:
+    def __init__(self):
+        # Init the list with a 'dummy' node which makes 
+        # removing a node from the beginning of list easier.
+        self.head = ListNode(-1)
+        self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        curr = self.head.next
+        i = 0
+        while curr:
+            if i == index:
+                return curr.val
+            i += 1
+            curr = curr.next
+        return -1  # Index out of bounds or list is empty
+
+    def insertHead(self, val: int) -> None:
+        new_node = ListNode(val)
+        new_node.next = self.head.next
+        self.head.next = new_node
+        if not new_node.next:  # If list was empty before insertion
+            self.tail = new_node
+
+    def insertTail(self, val: int) -> None:
+        self.tail.next = ListNode(val)
+        self.tail = self.tail.next
+
+    def remove(self, index: int) -> bool:
+        i = 0
+        curr = self.head
+        while i < index and curr:
+            i += 1
+            curr = curr.next
+        
+        # Remove the node ahead of curr
+        if curr and curr.next:
+            if curr.next == self.tail:
+                self.tail = curr
+            curr.next = curr.next.next
+            return True
+        return False
+
+    def getValues(self) -> List[int]:
+        curr = self.head.next
+        res = []
+        while curr:
+            res.append(curr.val)
+            curr = curr.next
+        return res
