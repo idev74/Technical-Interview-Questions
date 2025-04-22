453ca6c22f5d21dd43e721bc723fb5472efbdd4e
idev74 <idevassy77@gmail.com>
2025-04-22 12:05:52 -0700

    added 10 problems

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Technical Interview Questions
 Leetcode-style questions with the problem and solution for technical interview prep
+
+[Neetcode](https://neetcode.io) used for many problems
--- factory_method_pattern.py
+++ factory_method_pattern.py
@@ -4,6 +4,20 @@ Implement the Factory Method design pattern.
 The Factory Method is a creational design pattern that provides an interface for creating objects in a superclass but allows subclasses to alter the type of objects that will be created.
 
 You are given code that includes a few vehicles types and their respective factories. Complete the factory method implementation such that each factory returns the correct vehicle.
+
+Example:
+
+VehicleFactory carFactory = new CarFactory();
+VehicleFactory truckFactory = new TruckFactory();
+VehicleFactory bikeFactory = new BikeFactory();
+
+Vehicle myCar = carFactory.createVehicle();
+Vehicle myTruck = truckFactory.createVehicle();
+Vehicle myBike = bikeFactory.createVehicle();
+
+myCar.getType();   // "Car"
+myTruck.getType(); // "Truck"
+myBike.getType();  // "Bike"
 '''
 
 # Solution
